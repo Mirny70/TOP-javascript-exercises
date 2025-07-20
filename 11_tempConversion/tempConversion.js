e0e5e546ba9535f734a95d9033354d5a8c108d00
Mirny70 <dmitrysmirnov70@gmail.com>
2025-07-20 15:58:36 +0300

    Function convertToCelsius and function convertToFahrenheit completed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
+console.log(convertToCelsius(-100));
+console.log(convertToFahrenheit(-10));
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(37.8));
+console.log(convertToCelsius(-73.3));
+console.log(convertToFahrenheit(14));
 
 // Do not edit below this line
 module.exports = {
