5b930358a04806dedb8b4b0dc76ceb5e344b3b35
Mirny70 <dmitrysmirnov70@gmail.com>
2025-09-26 21:11:12 +0300

    feat(findTheOldest): calculate age correctly when yearOfDeath is missing
    - Add people parameter to the findTheOldest function
    - Use current year as yearOfDeath if it is undefined for oldestPerson
    - Compare age based on yearOfDeath and yearOfBirth for each person
    - Update tests to enable skipped cases covering undefined yearOfDeath scenarios
    - Ensure function returns the person with the greatest age correctly

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        if (!oldestPerson.yearOfDeath) {
+            oldestPerson.yearOfDeath = new Date().getFullYear();
+        }
+        const oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        const currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        return oldestAge > currentAge ? oldestPerson : currentPerson;
+    }, {});
 };
 
 // Do not edit below this line
